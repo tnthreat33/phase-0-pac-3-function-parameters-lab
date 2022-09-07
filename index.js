3a467996faaccd0d8b8ceea101906443ef51459a
Rachel Hamby <tnthreat33@gmail.com>
2022-09-07 15:38:07 -0400

    commit tree fa2fd1af8234351e5681f02e41b68069d1f646c8

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction (name){
+    return `Hi, my name is ${name}.`;
+}
+function introductionWithLanguage(name, language) {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.`
+  }
+introductionWithLanguage("Aki","Ember.js.")
+  function introductionWithLanguageOptional(name, language="Java Script") {
+    return`Hi, my name is ${name} and I am learning to program in ${language}.;}
